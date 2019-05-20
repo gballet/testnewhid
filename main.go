436a5bbd56af5c45e730f6934d56dd47f38474ec
Guillaume Ballet <gballet@gmail.com>
2019-05-20 19:56:47 +0200

    Match API update

--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"fmt"
 
-	"github.com/gballet/hid"
+	"github.com/karalabe/hid"
 )
 
 func main() {
-	fmt.Println(hid.Enumerate(0x2c97, 0x0001))
-	fmt.Println(hid.Enumerate(0x534c, 0x0001))
-	fmt.Println(hid.Enumerate(0x1209, 0x53c1))
+	candidates, err := hid.Enumerate(0x2c97, 0x0001)
+	if err == nil {
+		for _, dev := range candidates {
+			fmt.Println(dev.GetPath())
+		}
+	}
+	candidates, err = hid.Enumerate(0x534c, 0x0001)
+	if err == nil {
+		for _, dev := range candidates {
+			fmt.Println(dev.GetPath())
+		}
+	}
+	candidates, err = hid.Enumerate(0x1209, 0x53c1)
+	if err == nil {
+		for _, dev := range candidates {
+			fmt.Println(dev.GetPath())
+		}
+	}
+
 }
